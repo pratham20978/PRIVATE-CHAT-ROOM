f011c317054ffeff8439594f60e804220ea16f73
pratham20978 <ps705399@gmail.com>
2024-03-14 17:00:12 +0530

    Modify CR_Connection.h

--- CR_Connection.h
+++ CR_Connection.h
@@ -10,28 +10,142 @@ namespace olc
     namespace net
     {   
         template<typename T>
-        class Connection {
+        class Connection : public enable_shared_from_this<Connection<T>>{
             protected:
                 // Socket
-                asio::ip::tcp::socket socket;
+                asio::ip::tcp::socket m_socket;
 
                 // the context for socket
                 asio::io_context& m_asioContext;
 
                 TSQueue<T> QMessageOut;
 
-                TSQueue<T> QMessaageIn;
+                TSQueue<T>& QMessageIn;
+
+                asio::ip::tcp::acceptor m_asioAcceptor;
+
+                thread m_threadContext;
+
+                Message m_msgTemporaryIn;
 
             public:
-                Connection(){}
-                virtual ~connection(){}
+                Connection(asio::io_context asioContext, asio::ip::tcp::socket socket,
+                TSQueue<Message>& qIn, uint16_t port): m_asioContext(asioContext), m_socket(move(socket)),
+                QMessageIn(qIn), m_asioAcceptor(m_asioContext, asio::ip::tcp::endpoint(asio::ip::tcp::v4(),port))
+                {
+                    // Starting to connect to client
+                    m_asioContext.run();
+                    m_threadContext = thread([this](){
+                        while(true){
+                            try{
+                                if(IsConnected()){
+                                    WaitForClientConnection();
+                                    ConnectToClient();
+                                }
+                                this_thread::sleep_for(chrono::seconds(1));
+                            }
+                            catch(exception& ec){
+                            
+                                // Something prohibited the other user from listening
+                                cerr<<"[SERVER] Exception: "<<ec.what()<<endl;
+                                break;
+                            }
+                            cout<<"[SERVER] Started!"<<endl;
+                        }
+                    }); 
+                    
+                }
+                virtual ~Connection(){
+                    m_asioContext.stop();
+                    if(m_threadContext.joinable()){
+                        m_threadContext.join();
+                    }
+                }
+
 
             
-            bool ConnectToClient();
-            bool Disconnect();
-            bool IsConnected() const;
+            void ConnectToClient(const asio::ip::tcp::resolver::results_type& endpoints){
 
-            bool send(const Message& msg);
-        }
+                asio::async_connect(m_socket,endpoints,
+                [this](error_code ec, asio::ip::tcp::endpoint endpoint){
+                    if(!ec){
+                        ReadMessage();
+                    }
+                }
+                );
+            }
+
+            void WaitForClientConnection(){
+
+                m_asioAcceptor.async_accept([this](error_code ec,asio::ip::tcp::socket socket){
+                    if(ec){
+                        // Error has occured during acceptance
+                        cout<<"[SERVER] New Connection Error: "<<ec.message()<<endl;
+                    }
+                });
+            }
+
+
+            void Disconnect(){
+                if(IsConnected()){
+                    post(m_asioContext,[this](){m_socket.close();});
+                }
+            }
+
+            bool IsConnected() const{
+                return m_socket.is_open();
+            }
+
+            void Send(const Message& msg){
+                post(m_asioContext,
+                [this,msg](){
+                    bool bWriteMessage = QMessageOut.empty();
+                    QMessageOut.push(msg);
+                    if(!bWriteMessage){
+                        WriteMessage();
+                    }
+                });
+            }
+
+            private:
+            void ReadMessage(){
+
+                async_read(m_socket, asio::buffer(&m_msgTemporaryIn,sizeof(Message)),
+                [this](error_code ec,size_t length){
+                    if(!ec){
+                        AddToIncomingQueue();
+                    }else{
+                        cout<<"["<<m_msgTemporaryIn.getUserID()<<"] Read Msg Fail"<<endl;
+                        m_socket.close();
+                    }
+                }
+                );
+
+            }
+            
+            void WriteMessage(){
+
+                async_write(m_socket, asio::buffer(QMessageOut.get().data(),QMessageOut.get().size()),
+                [this](error_code ec,size_t length){
+                    if(!ec){
+                        QMessageOut.pop();
+                        if(!QMessageOut.empty()){
+                            WriteMessage();
+                        }
+                    }else{
+                        cout<<"["<<QMessageOut.get().getUserID()<<"] Write Msg Fail."<<endl;
+                        m_socket.close();
+                    }
+                }
+                );
+
+            }
+
+            void AddToIncomingQueue(){
+                QMessageIn.push(m_msgTemporaryIn);
+
+                ReadMessage();
+            }
+        };
     }
 }
